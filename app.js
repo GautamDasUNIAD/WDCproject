cb82ebe76e33d45cec9d2982e40133c8c37679b4
glebe <a1887500@adelaide.edu.au>
2024-06-06 01:29:39 +0930

    new message

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const passport = require('passport');
+var mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const passport = require('./utils/passport-config');
+require('dotenv').config();
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,14 +25,25 @@ var dbConnectionPool = require('./db');
 
 var app = express();
 
+app.use(function(req, res, next){
+    req.pool = dbConnectionPool;
+    next();
+});
+
+
 app.use(logger('dev'));
 app.use(express.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/stylesheets', express.static(path.join(__dirname, 'stylesheets')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+const { hashPassword, checkPassword } = require('../utils/password');
 
+
+const VALID_ROLES = ['Regular', 'Manager', 'Admin'];
 /* GET users listing. */
+//! remove
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  if (req.isAuthenticated()) {
+        res.json({ authenticated: true, user: req.user });
+    } else {
+        res.json({ authenticated: false });
+    }
+
+});
+
+router.post('/register', (req, res) => {
+  const { first_name, last_name, email, password, role, social_media_linked } = req.body;
+
+  // Validate password length
+  if (password.length < 8 || password.length > 64) {
+    return res.status(400).send('Password must be between 8 and 64 characters');
+  }
+
+  // Validate role
+  if (!VALID_ROLES.includes(role)) {
+    return res.status(400).send(`Role must be one of: ${VALID_ROLES.join(', ')}`);
+  }
+
+  try {
+    const hashedPassword = hashPassword(password);
+    req.pool.query('INSERT INTO Users (first_name, last_name, email, password, role, social_media_linked) VALUES (?, ?, ?, ?, ?, ?)',
+    [first_name, last_name, email, hashedPassword, role, social_media_linked], (err, results) => {
+      if (err) {
+        console.error('Error creating user:', err);
+        res.status(500).send('Error creating user');
+      } else {
+        res.status(201).json({ id: results.insertId, first_name, last_name, email, role, social_media_linked });
+      }
+    });
+  } catch (err) {
+    console.error('Error hashing password:', err);
+    res.status(500).send('Internal server error');
+  }
+});
+
+// Login user
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/',
+  failureFlash: true // Enable flash messages
+}));
+
+router.get('/logout', (req, res, next) => {
+  req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+  });
 });
 
 module.exports = router;
