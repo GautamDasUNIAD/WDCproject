3f07fa2d83e9435e708d814afb63937a0565742c
glebe <a1887500@adelaide.edu.au>
2024-06-08 02:28:03 +0930

    glebe ui changes

--- routes/index.js
+++ routes/index.js
@@ -43,5 +43,64 @@ router.get('/events', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'events.html'));
 });
 
+router.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'admin.html'));
+});
+
+router.get('/create-branch', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'create-branch.html'));
+});
+
+router.get('/create-event', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'create-event.html'));
+});
+
+router.get('/join-organization', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'join-organization.html'));
+});
+router.get('/managers', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'managers.html'));
+});
+
+router.get('/post-update', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'post-update.html'));
+});
+
+router.get('/preferences', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'preferences.html'));
+});
+
+router.get('/view-branch-users', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'view-branch-users.html'));
+});
+
+router.get('/view-branches', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'view-branches.html'));
+});
+
+router.get('/view-events', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'view-events.html'));
+});
+
+router.get('/view-join-branches', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'view-join-branches.html'));
+});
+
+router.get('/view-organization-users', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'view-organization-users.html'));
+});
+
+router.get('/view-rsvps', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'view-rsvps.html'));
+});
+
+router.get('/view-updates', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'view-updates.html'));
+});
+
+
+
+
+
 
 module.exports = router;
